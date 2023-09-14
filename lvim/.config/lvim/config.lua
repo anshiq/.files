d07ff00fc0b055dc4333dbb007f1431b904a423c
Anshiq <66484628+anshiq@users.noreply.github.com>
2023-09-14 16:54:52 +0530

    Update config.lua

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,4 +1,9 @@
+lvim.keys.normal_mode["L"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["H"] = ":BufferLineCyclePrev<CR>"
+lvim.format_on_save.enabled = true
 lvim.colorscheme = "dracula"
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 lvim.plugins = {
   "lunarvim/darkplus.nvim",
   'neovim/nvim-lspconfig',
@@ -6,12 +11,12 @@ lvim.plugins = {
   'MunifTanjim/prettier.nvim',
   'MunifTanjim/eslint.nvim',
   'Mofiqul/dracula.nvim',
-  {
-    url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-    config = function()
-      require("lsp_lines").setup()
-    end,
-  }
+  -- {
+  --   url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  --   config = function()
+  --     require("lsp_lines").setup()
+  --   end,
+  -- }
   -- {
   --       "tzachar/cmp-tabnine",
   --       run = "./install.sh",
@@ -29,30 +34,43 @@ lvim.plugins = {
   --     },
 
 }
-vim.diagnostic.config({
-  virtual_text = false,
-})
-local null_ls = require("null-ls")
-local eslint = require("eslint")
+-- vim.diagnostic.config({
+--   virtual_text = false,
+-- })
+-- local null_ls = require("null-ls")
+-- local eslint = require("eslint")
 
-null_ls.setup()
 
-eslint.setup({
-  bin = 'eslint', -- or `eslint_d`
-  code_actions = {
-    enable = true,
-    apply_on_save = {
-      enable = true,
-      types = { "directive", "problem", "suggestion", "layout" },
+-- null_ls.setup()
+
+-- eslint.setup({
+--   bin = 'eslint', -- or `eslint_d`
+--   code_actions = {
+--     enable = true,
+--     apply_on_save = {
+--       enable = true,
+--       types = { "directive", "problem", "suggestion", "layout" },
+--     },
+--     disable_rule_comment = {
+--       enable = true,
+--       location = "separate_line", -- or `same_line`
+--     },
+--   },
+--   diagnostics = {
+--     enable = true,
+--     report_unused_disable_directives = false,
+--     run_on = "type", -- or `save`
+--   },
+-- })
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+    { command = "eslint", filetypes = { "typescript", "typescriptreact" } }
+}
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+    {
+        command = "prettier",
+        filetypes = { "typescript", "typescriptreact" },
     },
-    disable_rule_comment = {
-      enable = true,
-      location = "separate_line", -- or `same_line`
-    },
-  },
-  diagnostics = {
-    enable = true,
-    report_unused_disable_directives = false,
-    run_on = "type", -- or `save`
-  },
-})
+}
